7f09bfeaad82d3961a71b874fa126b5c3e434895
merabza <merabza@gmail.com>
2026-06-13 09:42:45 +0400

    202606130942

---
+++ CrawlerRepositories/CrawlerRepositoryCreatorFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CrawlerRepoInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrawlerRepositories;
+
+public sealed class CrawlerRepositoryCreatorFactory : ICrawlerRepositoryCreatorFactory
+{
+    private readonly IServiceProvider _services;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public CrawlerRepositoryCreatorFactory(IServiceProvider services)
+    {
+        _services = services;
+    }
+
+    public ICrawlerRepository GetCrawlerRepository()
+    {
+        // ReSharper disable once using
+        using IServiceScope scope = _services.CreateScope();
+        return scope.ServiceProvider.GetRequiredService<ICrawlerRepository>();
+    }
+}
