bdad24990c5f413cce372a8a731f9192c14c3929
merabza <merabza@gmail.com>
2026-06-13 08:28:17 +0400

    first commit

---
+++ CrawlerDbPersistence/Configurations/SchemeModelConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CrawlerDbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SystemTools.SystemToolsShared;
+
+namespace CrawlerDbPersistence.Configurations;
+
+public sealed class SchemeModelConfiguration : IEntityTypeConfiguration<SchemeModel>
+{
+    public const int SchemeNameLength = 50;
+
+    public void Configure(EntityTypeBuilder<SchemeModel> builder)
+    {
+        const string tableName = "Schemes";
+        builder.ToTable(tableName);
+
+        builder.HasKey(e => e.SchId);
+        builder.HasIndex(e => e.SchName).IsUnique();
+
+        builder.Property(e => e.SchName).HasMaxLength(SchemeNameLength);
+        builder.Property(e => e.SchProhibited).HasDefaultValue(0);
+    }
+}
